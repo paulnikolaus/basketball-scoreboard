3a1ffada145467f2ab0f4584ae303692277d09cc
Paul Nikolaus <pnikolaus@mailbox.org>
2026-02-15 23:11:57 +0100

    Update to new Gradle Version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,7 @@ dependencies {
     // Test dependencies
     // ----------------------------
 
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
-    testImplementation("app.cash.turbine:turbine:1.2.1")
+    testImplementation(libs.junit)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.turbine)
 }
